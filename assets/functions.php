34f17e7741ced5a0048d289fad1043e2a407760f
Secri <44509048+Secri@users.noreply.github.com>
2023-03-24 12:43:55 +0100

    Update functions.php

--- assets/functions.php
+++ assets/functions.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * Add my new menu to the Admin Control Panel
+ * Ajouter le menu de paramétrage du widget à la page Admin
  */
  
-// Hook the 'admin_menu' action hook, run the function named 'mfp_Add_My_Admin_Link()'
+// Hook the 'admin_menu' action hook
 add_action( 'admin_menu', 'Add_secriwff_Admin_Link' );
  
 // Add a new top level menu link to the ACP
@@ -14,10 +14,10 @@ function Add_secriwff_Admin_Link()
         'Fb widget feed', // Text to show on the menu link
         'manage_options', // Capability requirement to see the link
         'secriwff-admin-page', // The 'slug' - file to display when clicking the link
-	      	'secriwff_admin_menu_page'
+		'secriwff_admin_menu_page'
     );
 }
 
 function secriwff_admin_menu_page () {
-	echo "<h1>Hello World!</h1>";
+	include plugin_dir_path( __FILE__ ) . './settings.php';
 }
