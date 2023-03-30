27931d9ee98b6e46f12af122018829ed1c6bf776
Secri <44509048+Secri@users.noreply.github.com>
2023-03-30 15:29:59 +0200

    Add files via upload

---
+++ widget-tw-feed.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Widget Twitter Feed
+Plugin URI: 
+Description: Widget affichant une timeline Twitter
+Authors: Christophe IENZER
+Version: 0.1
+Author URI: https://www.linkedin.com/in/christophe-ienzer/
+*/
+
+/* Chargement des ressources PHP pour gérer le plugin */
+require_once plugin_dir_path(__FILE__) . 'assets/functions.php'; //gestion des metabox
+require_once plugin_dir_path(__FILE__) . 'assets/widget.php'; //création du widget associé au plugin
