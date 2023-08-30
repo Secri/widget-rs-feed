b6cd8ae567363d76c776376badfc320834634114
Secri <44509048+Secri@users.noreply.github.com>
2023-08-30 17:10:30 +0200

    Update widget-tw-feed.php

--- widget-tw-feed.php
+++ widget-tw-feed.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Plugin Name: Widget Twitter Feed
+Plugin Name: Widget RS Feed
 Plugin URI: 
-Description: Widget affichant une timeline Twitter
+Description: Widget affichant des timelines de réseaux sociaux
 Authors: Christophe IENZER
-Version: 0.1
+Version: 0.2
 Author URI: https://www.linkedin.com/in/christophe-ienzer/
 */
 
 /* Chargement des ressources PHP pour gérer le plugin */
-require_once plugin_dir_path(__FILE__) . 'assets/functions.php';
+require_once plugin_dir_path(__FILE__) . 'assets/functions.php'; //gestion des metabox
 require_once plugin_dir_path(__FILE__) . 'assets/widget.php'; //création du widget associé au plugin
