540bec07972455412451c9276c02500761b25243
Secri <44509048+Secri@users.noreply.github.com>
2023-11-21 17:29:20 +0100

    Update functions.php

--- assets/functions.php
+++ assets/functions.php
@@ -1,3 +1,10 @@
 <?php
 
 include plugin_dir_path( __FILE__ ) . './settings.php';
+
+/* Active les taxonomies par défaut sur le post type Page */
+function share_taxo_with_pages() {
+	register_taxonomy_for_object_type( 'category', 'page' );
+}
+
+add_action( 'init', 'share_taxo_with_pages' );
