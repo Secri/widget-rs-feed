762abc58be6e290ea41789d1cbfd52119affb864
Secri <44509048+Secri@users.noreply.github.com>
2023-11-22 14:06:01 +0100

    Update functions.php

--- assets/functions.php
+++ assets/functions.php
@@ -3,8 +3,8 @@
 include plugin_dir_path( __FILE__ ) . './settings.php';
 
 /* Active les taxonomies par défaut sur le post type Page */
-function share_taxo_with_pages() {
+function wrsf_share_taxo_with_pages() {
 	register_taxonomy_for_object_type( 'category', 'page' );
 }
 
-add_action( 'init', 'share_taxo_with_pages' );
+add_action( 'init', 'wrsf_share_taxo_with_pages' );
